eccae7185dfb8a2ea2b5f031999323587c8490d9
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-03-28 15:00:24 +0300

    Add some styling to task list.

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,10 +1,23 @@
 <template>
-  <tr v-for="task in tasks" :key="task.name">
-    <td>{{ task.name }}</td>
-    <td>{{ task.note }}</td>
-    <td>{{ task.points }}</td>
-    <td>{{ task.room }}</td>
-  </tr>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Note</th>
+        <th>Points</th>
+        <th>Room</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="task in tasks" :key="task.name">
+        <td>{{ task.name }}</td>
+        <td>{{ task.note }}</td>
+        <td>{{ task.points }}</td>
+        <td>{{ task.room }}</td>
+        <td><BaseButton>Edit</BaseButton><BaseButton>Delete</BaseButton></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
