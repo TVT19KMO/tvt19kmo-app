16dd89e0c95494626bacde548455a19b136ca5f2
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-03-28 10:03:17 +0300

    Create project skeleton.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">asdasd</div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped lang="postcss">
+.wrapper {
+  @apply text-red-50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
