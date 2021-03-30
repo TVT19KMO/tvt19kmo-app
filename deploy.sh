7cfb98ac1febabcbd8b63b8bdfdfb60d4680c876
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-03-30 07:33:44 +0300

    Add initial deployment pipeline.

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:tvt19kmo/tvt19kmo-app.git dev:gh-pages
+
+cd -
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+  base: '/tvt19kmo-app/',
   plugins: [vue()],
   resolve: {
     alias: {
