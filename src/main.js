6c2410f3aa4d9e0777c38891c90e129db5a64530
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-01 13:06:10 +0300

    Create BaseForm component and add styling.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue';
 
 import BaseInput from '@/components/BaseInput.vue';
 import BaseButton from '@/components/BaseButton.vue';
+import BaseForm from '@/components/BaseForm.vue';
+
 import App from './App.vue';
 import './index.css';
 
 createApp(App)
   .component('BaseButton', BaseButton)
   .component('BaseInput', BaseInput)
+  .component('BaseForm', BaseForm)
   .mount('#app');
