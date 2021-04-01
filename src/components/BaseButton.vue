247d09c445df48a6f8844a63678c78afaffea7e4
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-01 12:57:40 +0300

    Create BaseButton component and add styling.

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="button"><slot></slot></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="postcss">
+.button {
+  @apply inline-block px-3 py-2 rounded-sm;
+  @apply bg-primary text-white font-semibold;
+  @apply text-center align-middle;
+}
+</style>
