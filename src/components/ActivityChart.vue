6e555f20efd51226c35b77d172e7141546b5c96f
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-01 13:21:10 +0300

    Create ActivityChart component for testing purposes.

---
+++ src/components/ActivityChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      /** @type {CanvasRenderingContext2D} */
+      ctx: null,
+    };
+  },
+  computed: {
+    width() {
+      return this.$refs.canvas.clientWidth;
+    },
+    height() {
+      return this.$refs.canvas.clientHeight;
+    },
+  },
+  mounted() {
+    this.ctx = this.$refs.canvas.getContext('2d');
+    this.draw();
+  },
+  methods: {
+    draw() {
+      this.ctx.textAlign = 'center';
+      this.ctx.fillText('Activity', 0, 0, this.width, this.height);
+    },
+  },
+};
+</script>
+
+<style></style>
