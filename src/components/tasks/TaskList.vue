de7dc2236beeb926f512d4d5de0d3cae2db07e44
Jaakko Ikäheimo <48987957+JIkaheimo@users.noreply.github.com>
2021-04-05 00:31:55 +0300

    Revert "first commit"

--- src/components/tasks/TaskList.vue
+++ src/components/tasks/TaskList.vue
@@ -1,4 +1,4 @@
-<template >
+<template>
   <BaseTable>
     <template #head>
       <th scope="col">Name</th>
@@ -6,35 +6,20 @@
       <th scope="col">Points</th>
       <th scope="col">Room</th>
     </template>
-
-    <template #default id="app">
-      <TaskListRow v-for="task in tasks" v-bind="task" :key="task.Name">
-        
-        <td> {{task.Name}}</td>
-        <td> {{task.Note}} </td>
-        <td> {{task.Points}} </td>
-        <td> {{task.Room}} </td>
-        
-          
-      </TaskListRow>
+    <template #default>
+      <TaskListRow v-for="task in tasks" v-bind="task" :key="task.name" />
     </template>
-
   </BaseTable>
 </template>
 
 <script>
-
-//import faker from 'faker';
+import faker from 'faker';
 import TaskListRow from './TaskListRow.vue';
-import axios from 'axios';
 
 export default {
-  el: '#app',
   components: { TaskListRow },
-  
-  data: function() {
+  data() {
     return {
-      /*
       tasks: Array.from({ length: 10 }).map((_) => ({
         name: faker.random.arrayElement([
           'Empty trash',
@@ -49,61 +34,9 @@ export default {
         points: faker.datatype.number(20),
         room: faker.random.arrayElement(['kitchen', 'living room', 'restroom']),
       })),
-      */
-     tasks: 
-     [
-        /*
-        {
-          name: 'TESTI1',
-          note: 'lorem ipsum bdbggfbgfbf bssssssssssss gbfffffffffffs gffffffffffffs',
-          points: 12,
-          room: 'Blogging with Vue'  
-        },
-        {
-          name: 'TESTI2',
-          note: 'lorem ipsum bdbggfbgfbf bssssssssssss gbfffffffffffs gffffffffffffs',
-          points: 12,
-          room: 'Blogging with Vue'  
-        }
-        */
-      ]
-
-      
     };
   },
-  
- mounted: function() {
-    axios
-      .get('http://localhost:5000/api/tasks')
-      .then((response) => {
-        //let resData =   JSON.stringify(response.data)
-       
-        let resData = response.data
-        //let jsObject = JSON.parse(resData)
-       
-        //this.tasks.push = resData
-          
-        
-        console.log(this.tasks.push.length)
-        console.log(typeof resData)
-
-        for(let i = 0; i < resData.length; i++) {
-            console.log(resData[i]
-            )
-            this.tasks.push(resData[i])
-        }
-        
-
-        return this.tasks
-        
-      })
-      .catch(error => {
-        console.log(error);
-        this.errored = true
-      })
-      .finally(() => this.loading = false)
-  }
-}
+};
 </script>
 
 <style></style>
