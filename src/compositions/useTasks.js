9bf6a4b6a9325926ea370ce7f048d284274e70fb
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-09 19:37:57 +0300

    Fix multiline functions.

--- src/compositions/useTasks.js
+++ src/compositions/useTasks.js
@@ -12,17 +12,13 @@ export const useTasks = () => {
 
   const getTasks = async () => await store.dispatch(types.GET_TASKS);
 
-  const selectTask = (id)
-    => store.commit(types.SELECT_TASK, id);
+  const selectTask = (id) => store.commit(types.SELECT_TASK, id);
 
-  const deleteTask = async (id)
-    => await store.dispatch(types.DELETE_TASK, id);
+  const deleteTask = async (id) => await store.dispatch(types.DELETE_TASK, id);
 
-  const updateTask = async (taskToUpdate)
-    => await store.dispatch(types.UPDATE_TASK, taskToUpdate);
+  const updateTask = async (taskToUpdate) => await store.dispatch(types.UPDATE_TASK, taskToUpdate);
 
-  const createTask = async (taskToCreate)
-    => await store.dispatch(types.CREATE_TASK, taskToCreate);
+  const createTask = async (taskToCreate) => await store.dispatch(types.CREATE_TASK, taskToCreate);
 
   return {
     tasks,
