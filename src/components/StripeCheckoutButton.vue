411f47b55a6dd6ff9f34a7ae7ce7b254c8ee810c
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-19 18:01:49 +0300

    Cleanup stripe button.

--- src/components/StripeCheckoutButton.vue
+++ src/components/StripeCheckoutButton.vue
@@ -5,7 +5,6 @@
 <script>
 import { useStripe } from 'vue-use-stripe';
 import { createCheckoutSession } from '@/api';
-import { useRoute } from 'vue-router';
 import { defineComponent } from 'vue-demi';
 
 export default defineComponent({
@@ -23,25 +22,19 @@ export default defineComponent({
   },
 
   setup({ itemToPurchase }) {
-    const route = useRoute();
-
     const { stripe } = useStripe({
       key:
         'pk_test_51Iebt0EEKvNzBMMmXI1hAPxVMzI04NnqMQ0hge5i7T0VSVGHcXOzHptqmZNa30HOmzXO30INkqxNe41rYOxTkH1K00yK0UM3Mr',
     });
 
     const checkout = async () => {
-      console.log(window.location);
       try {
-        console.log(itemToPurchase);
         const {
           data: { id },
         } = await createCheckoutSession(itemToPurchase, {
           route: window.location.href,
         });
-        console.log(window.location);
         const data = await stripe.value.redirectToCheckout({ sessionId: id });
-        console.log(data);
       } catch (e) {
         console.log(e);
       }
