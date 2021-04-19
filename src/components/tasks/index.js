2b38dbd74a56fc35d04788c53968b2d3b90859da
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-19 17:59:59 +0300

    Add child store and general store.

---
+++ src/components/tasks/index.js
@@ -0,0 +1,11 @@
+import TaskAssignModal from './TaskAssignModal.vue';
+import TaskEditModal from './TaskEditModal.vue';
+import TaskList from './TaskList.vue';
+
+export { TaskAssignModal, TaskEditModal, TaskList };
+
+export default {
+  TaskAssignModal,
+  TaskEditModal,
+  TaskList,
+};
---
+++ src/store/children/state.js
@@ -0,0 +1,5 @@
+export const state = {
+  children: [],
+};
+
+export default state;
