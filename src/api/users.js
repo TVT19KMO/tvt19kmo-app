bc92284803f1739149add9f31110279dd348a9e4
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-19 17:56:08 +0300

    Add missing API paths.

--- src/api/users.js
+++ src/api/users.js
@@ -1,11 +1,13 @@
 import { api } from './setup';
 
-export const registerUser = async (user) => api.post('users/register', user);
+export const registerUser = async (user) => await api.post('users/register', user);
 
-export const authenticateUser = async (user) => api.post('users/token', user);
+export const loginUser = async (user) => await api.post('users/login', user);
 
-export const loginUser = async (user) => api.post('users/login', user);
+export const getInfo = async () => await api.get('users/info');
 
 export default {
   registerUser,
+  loginUser,
+  getInfo,
 };
