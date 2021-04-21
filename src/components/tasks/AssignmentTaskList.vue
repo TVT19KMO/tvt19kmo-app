0b577ef2373dec49652943087d81c8c9195fb1c1
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-21 21:55:22 +0300

    Create component for displaying assigned tasks.

---
+++ src/components/tasks/AssignmentTaskList.vue
@@ -0,0 +1,47 @@
+<template>
+  <DataView layout="list" :paginator="true" :rows="5" :value="tasks">
+    <template #header>
+      <div class="p-grid p-nogutter">Assigned Tasks</div>
+    </template>
+
+    <template #list="{ data: { task, assignee, assigned, id } }">
+      <div class="p-5 w-full">
+        <div class="flex space-x-5 justify-between">
+          <div>
+            <div class="text-lg font-bold">{{ task.name }}</div>
+            <i class="pi pi-user product-category-icon"></i
+            ><span class="product-category">{{ assignee.name }}</span>
+          </div>
+          <div class="flex space-x-2 items-center">
+            <Tag :value="task.room.name" rounded />
+            <Tag :value="task.difficulty.name" rounded />
+          </div>
+          <div class="space-x-3">
+            <span>{{ assigned }}</span>
+            <Button
+              v-if="!task.completed"
+              icon="pi pi-check"
+              @click="$emit('complete', id)"
+              label="Ready"
+            ></Button>
+          </div>
+        </div>
+      </div>
+    </template>
+  </DataView>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  emits: ['complete'],
+};
+</script>
+
+<style></style>
