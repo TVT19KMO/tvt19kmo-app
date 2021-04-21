8e413c160604ff5f2e6cf934381db220a7a272ba
THeikki <t9tohe00@students.oamk.fi>
2021-04-21 17:18:42 +0300

    updated

--- src/api/children.js
+++ src/api/children.js
@@ -2,13 +2,12 @@ import { api } from './setup';
 
 export const getChildren = async () => await api.get('children');
 
-
-/*
 module.exports = {
   getChildren,
 };
-*/
 
+/*
 export default {  
   getChildren
 }
+*/
